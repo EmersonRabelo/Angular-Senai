b32630f235dd5f3e58556e2b315b6e7cdbfe800b
Emerson Rabelo <emersonrabelo1998@gmail.com>
2022-02-19 01:11:42 -0300

    add: mural component

--- vagas/src/app/mural-vagas/mural-vagas.component.ts
+++ vagas/src/app/mural-vagas/mural-vagas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Vaga } from '../models/Vagas.model';
+import { VagasService } from '../vagas.service';
 
 @Component({
   selector: 'app-mural-vagas',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MuralVagasComponent implements OnInit {
 
-  constructor() { }
+  public vagas: Vaga[] = [];
+  public vaga: Vaga = new Vaga(0, "", "", "", 0);
+
+
+  constructor(private _vagaService: VagasService) { }
 
   ngOnInit(): void {
+    this.listarVagas();
+  }
+
+  listarVagas() {
+    this._vagaService.getVagas()
+      .subscribe(
+        retornaVaga => {
+          this.vagas = retornaVaga.map(
+            item  => {return new Vaga(
+              item.id,
+              item.nome,
+              item.foto,
+              item.descricao,
+              item.salario
+            );}
+          )
+        }
+      )
+  }
+
+  excluir(id: Number) {
+    this._vagaService.removeVagas(id).subscribe(
+      vaga => {this.vaga = new Vaga(0, "", "", "", 0)},
+      err => {console.log("erro ao excluir")}
+    );
+
+    window.location.href = "/mural";
+
   }
 
 }
