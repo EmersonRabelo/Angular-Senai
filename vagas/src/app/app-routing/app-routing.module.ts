87a7e6f23fa1439822c59c3f4d70d37eabc75248
Emerson Rabelo <emersonrabelo1998@gmail.com>
2022-02-19 01:14:48 -0300

    add: angular routing

---
+++ vagas/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule} from '@angular/router';
+
+import { MuralVagasComponent } from '../mural-vagas/mural-vagas.component';
+import { PainelVagasComponent } from '../painel-vagas/painel-vagas.component';
+
+const rotas: Routes = [
+    {path: 'mural', component: MuralVagasComponent},
+    {path: 'painel', component: PainelVagasComponent},
+    {path: '', redirectTo: '/mural', pathMatch: 'full'} // Caso não seja escolhi nenhuma das opções acima, sera direcionado a mural
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(rotas)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
