51c8f3a48bddd11683101d27e510d36bdc55ef36
Emerson Rabelo <emersonrabelo1998@gmail.com>
2022-02-19 01:07:49 -0300

    Update: class Vagas

--- vagas/src/app/models/Vagas.model.ts
+++ vagas/src/app/models/Vagas.model.ts
@@ -1,4 +1,4 @@
-class Vaga {
+export class Vaga {
 
     id: Number = 0;
     nome: String = "";
@@ -6,7 +6,13 @@ class Vaga {
     descricao: String = "";
     salario: Number = 0;
 
-    
+    constructor(id: Number, nome: String, foto: String, descricao: String, salario: Number){
+        this.id = id;
+        this.nome= nome;
+        this.foto = foto;
+        this.descricao = descricao;
+        this.salario = salario;
+    }
 
 
 }
