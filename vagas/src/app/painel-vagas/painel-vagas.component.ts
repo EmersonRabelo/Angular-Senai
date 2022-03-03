4716d90d11da04be8e1742f96815bdb1c1a8e320
Emerson Rabelo <emersonrabelo1998@gmail.com>
2022-03-03 00:53:25 -0300

    update: painel-vagas component

--- vagas/src/app/painel-vagas/painel-vagas.component.ts
+++ vagas/src/app/painel-vagas/painel-vagas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Vaga } from '../models/Vagas.model';
+import { VagasService } from '../vagas.service';
 
 @Component({
   selector: 'app-painel-vagas',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelVagasComponent implements OnInit {
 
-  constructor() { }
+  public vaga: Vaga = new Vaga(0, "", "", "", 0);
+
+  constructor(private _vagaService: VagasService) { }
 
   ngOnInit(): void {
+  }
+
+  atualizar() {
+    var id = localStorage.getItem('id')
+    this._vagaService.atualizarVagas(id, this.vaga).subscribe(
+      vaga => {this.vaga = new Vaga(0, "", "", "", 0)},
+      err => {console.log("erro ao cadastrar")}
+    );
+      alert("VAGA ATUALIZADA");
+      localStorage.removeItem('id');
+      localStorage.clear();
+      
+      window.location.href = "/mural";
   }
 
 }
